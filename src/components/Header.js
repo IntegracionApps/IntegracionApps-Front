e88c608a313ec3d4d921eb58fb70da88d29e26e9
DamiMonta <damian_montagnoli@hotmail.com>
2021-08-13 10:04:45 -0300

    carpeta componentes

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles((theme) => ({
+
+    root: {
+        // display: "flex",
+    },
+    header: {
+        display: "flex",
+        justifyContent: "space-between",
+        backgroundColor: "#FFAA56",
+        // flexDirection: "row",
+    }
+
+}));
+
+
+export default function Header(props){
+    return (
+        <div className={useStyles().root}>
+            <div className={useStyles().header}>
+                <a>Título</a>
+                <a>Imagen? Barra de búsqueda?</a>
+                <a>sidebar button</a>
+            </div>
+            <a>grid</a>
+
+        </div>
+    )
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,32 +1,10 @@
 import React from "react";
-import { makeStyles } from '@material-ui/styles';
-
-const useStyles = makeStyles((theme) => ({
-
-    root: {
-        // display: "flex",
-    },
-    header: {
-        display: "flex",
-        justifyContent: "space-between",
-        backgroundColor: "#FFAA56",
-        // flexDirection: "row",
-    }
-
-}));
+import Header from "../components/Header"
 
 
 export default function Home() {
 
     return (
-        <div className={useStyles().root}>
-            <div className={useStyles().header}>
-                <a>Título aaaaaaaaaaa</a>
-                <a>Imagen? Barra de búsqueda?</a>
-                <a>sidebar button</a>
-            </div>
-            <a>grid</a>
-
-        </div>
+        <Header></Header>
     )
 }
