3960cabe85ad6bf8764c7a4cdaa20fecd3d0ffe1
MelodRAMAtick <melodramatick@users.noreply.github.com>
2021-09-07 00:57:14 -0300

    formulario_venta con formulario v0.1

--- src/pages/formulario_venta.js
+++ src/pages/formulario_venta.js
@@ -1,16 +1,128 @@
-import React from "react";
+import { TextField, FormControl, Input, InputLabel, FormHelperText } from "@material-ui/core";
+import React, { useState } from "react";
 import { useHistory } from "react-router";
 import Header from "../components/Header";
 
-export default function NuevaVenta(){
-    const history= useHistory();
+export default function NuevaVenta() {
+    const history = useHistory();
+    const [values, setValues] = useState({
+        name: '',
+        lastName: '',
+        address: '',
+        floor: '',
+        dni: null,
+        mail: '',
+        phone: null,
+    })
 
+    const handleName = (event) => {
+        event.preventDefault();
+        setValues((values) => ({
+            ...values,
+            name: event.target.value,
+        }));
+    };
 
-    return(
+    const handleLastName = (event) => {
+        event.preventDefault();
+        setValues((values) => ({
+            ...values,
+            lastName: event.target.value,
+        }));
+    };
+
+    const handleAddress = (event) => {
+        event.preventDefault();
+        setValues((values) => ({
+            ...values,
+            address: event.target.value,
+        }));
+    };
+
+    const handleFloor = (event) => {
+        event.preventDefault();
+        setValues((values) => ({
+            ...values,
+            floor: event.target.value,
+        }));
+    };
+
+    const handleDni = (event) => {
+        event.preventDefault();
+        setValues((values) => ({
+            ...values,
+            dni: event.target.value,
+        }));
+    };
+
+    const handleMail = (event) => {
+        event.preventDefault();
+        setValues((values) => ({
+            ...values,
+            mail: event.target.value,
+        }));
+    };
+
+    const handlePhone = (event) => {
+        event.preventDefault();
+        setValues((values) => ({
+            ...values,
+            phone: event.target.value,
+        }));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(values.name + " " + values.lastName + " " + values.address + " " + values.floor + " " + values.dni + " " + values.mail + " " + values.phone);
+    }
+    return (
         <div>
-            <Header/>
+            <Header />
             <h1>Nueva Venta</h1>
-            <a onClick={() =>{history.goBack()}} style={{color:"blue"}}>Volver</a>
+            <form autoComplete="off" noValidate onSubmit={handleSubmit}>
+                <div style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "auto" }}>
+                    <FormControl autoComplete="off" required="true">
+                        <InputLabel htmlFor="name">Nombre(s)</InputLabel>
+                        <Input id="name" value={values.name} onChange={handleName} />
+                        {/* <TextField required onChange={} id="names" label="Nombre(s)" value="" placeholder="p.ej. Juan Andrés" /> */}
+                    </FormControl>
+                    <FormControl autoComplete="off" required="true">
+                        <InputLabel htmlFor="lastName">Apellido(s)</InputLabel>
+                        <Input id="lastName" value={values.lastName} onChange={handleLastName} />
+                        {/* <TextField required onChange={handleLastName(event)} id="last-name" label="Apellido" placeholder="p.ej. Pérez de Villar" /> */}
+                    </FormControl>
+                    <FormControl autoComplete="off" required="true">
+                        <InputLabel htmlFor="address">Dirección</InputLabel>
+                        <Input id="address" value={values.address} onChange={handleAddress} />
+                        {/* <TextField required onChange={handleAddress(event)} id="address" label="Dirección" placeholder="p.ej Pueyrredón 2048" /> */}
+                    </FormControl>
+                    <FormControl autoComplete="off" >
+                        <InputLabel htmlFor="floor">Piso</InputLabel>
+                        <Input id="floor" value={values.floor} onChange={handleFloor} />
+                        {/* <TextField onChange={handleFloor(event)} id="floor" label="Piso" placeholder="14A" /> */}
+                    </FormControl>
+                    <FormControl autoComplete="off" required="true">
+                        <InputLabel htmlFor="dni">DNI</InputLabel>
+                        <Input id="dni" value={values.dni} onChange={handleDni} />
+                        {/* <TextField required onChange={handleDni(event)} id="dni" label="DNI" /> */}
+                    </FormControl>
+                    <FormControl autoComplete="off" required="true">
+                        <InputLabel htmlFor="mail">E-Mail</InputLabel>
+                        <Input id="mail" value={values.mail} onChange={handleMail} type="email" />
+                        {/* <TextField required onChange={handleMail(event)} id="mail" label="E-Mail" placeholder="ejemplo.example@email.com" /> */}
+                    </FormControl>
+                    <FormControl autoComplete="off" >
+                        <InputLabel htmlFor="phone">Teléfono</InputLabel>
+                        <Input id="phone" value={values.phone} onChange={handlePhone} type="phone" />
+                        <FormHelperText>¡Incluir código de área también!</FormHelperText>
+                        {/* <TextField onChange={handlePhone(event)} id="phone" label="Teléfono" helperText="¡Incluir código de área!" /> */}
+                    </FormControl>
+                    <FormControl >
+                        <Input type="submit" value="Confirmar Compra" />
+                    </FormControl>
+                </div>
+            </form>
+            <a onClick={() => { history.goBack() }} style={{ color: "blue" }}>Volver</a>
         </div>
     )
 }
