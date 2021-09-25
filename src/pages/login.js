66db7f6c5667632a8535e0d840508f0125e1c215
MelodRAMAtick <melodramatick@users.noreply.github.com>
2021-09-25 04:08:47 -0300

    Changed DNI to password for verification

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,95 +1,144 @@
+import { Button } from "@material-ui/core";
 import Lock from "@material-ui/icons/Lock";
 import Mail from "@material-ui/icons/Mail";
 import axios from "axios";
+import * as yup from 'yup';
+import { Field, Formik, Form } from "formik";
 import React, { useState } from "react";
 import { useHistory } from "react-router";
 import "../styles/Login.css";
 
+const validationSchema = yup.object({
+
+    email: yup
+        .string()
+        .email('Ingrese un e-mail válido')
+        .required('¡Este campo es obligatorio!'),
+
+    password: yup
+        .string()
+        .required('La contraseña es obligatoria'),
+
+});
+
+
 
 const Login = () => {
-    const [values, setValues] = useState({
-        mail: "",
-        pass: "",
-    });
+    // const [values, setValues] = useState({
+    //     mail: "",
+    //     password: "",
+    // });
 
     const history = useHistory();
 
     localStorage.setItem("finished", false);
 
-    const handleMail = (event) => {
-        event.preventDefault();
-        setValues((values) => ({
-            ...values,
-            mail: event.target.value,
-        }));
+    // const handleMail = (event) => {
+    //     event.preventDefault();
+    //     setValues((values) => ({
+    //         ...values,
+    //         mail: event.target.value,
+    //     }));
 
-    }
+    // }
 
-    const handlePass = (event) => {
-        event.preventDefault();
-        setValues((values) => ({
-            ...values,
-            pass: event.target.value,
-        }));
+    // const handlePass = (event) => {
+    //     event.preventDefault();
+    //     setValues((values) => ({
+    //         ...values,
+    //         pass: event.target.value,
+    //     }));
 
-    }
+    // }
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        axios.post('http://localhost:5000/Users/get/' + values.mail, {
-            password: values.pass,
-        })
-            .then((res) => {
-                switch (res.status) {
-                    case 200:
-                        console.log(res.data);
-                        if (res.data !== false) {
-                            if (res.data.rol === "Administrador") window.localStorage.setItem("rol", 0);
-                            if (res.data.rol === "Cajero") window.localStorage.setItem("rol", 1);
-                            if (res.data.rol === "Cliente") window.localStorage.setItem("rol", 2);
-                            history.push("/Home");
-                        }
-                        else alert("Datos ingresados incorrectos");
-                        break;
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     axios.post('http://localhost:5000/Users/get/' + values.mail, {
+    //         password: values.pass,
+    //     })
+    //         .then((res) => {
+    //             switch (res.status) {
+    //                 case 200:
+    //                     console.log(res.data);
+    //                     if (res.data !== false) {
+    //                         if (res.data.rol === "Administrador") window.localStorage.setItem("rol", 0);
+    //                         if (res.data.rol === "Cajero") window.localStorage.setItem("rol", 1);
+    //                         if (res.data.rol === "Cliente") window.localStorage.setItem("rol", 2);
+    //                         history.push("/Home");
+    //                     }
+    //                     else alert("Datos ingresados incorrectos");
+    //                     break;
 
-                }
-            })
-            .catch((err) => {
-                switch (err.response.status) {
-                    case 404: alert(err.response.data);
-                        break;
-                    case 500: console.log(err);
-                        break;
-                }
-            });
-        // console.log(res.data);
-        // history.push("/Home");
+    //             }
+    //         })
+    //         .catch((err) => {
+    //             switch (err.response.status) {
+    //                 case 404: alert(err.response.data);
+    //                     break;
+    //                 case 500: console.log(err);
+    //                     break;
+    //             }
+    //         });
+    //     // console.log(res.data);
+    //     // history.push("/Home");
 
-    }
+    // }
     return (
         <body>
             <div className="container_login">
                 <div className="forms-container">
-                    <form action="" className="sign-in-form" onSubmit={handleSubmit}>
-                        <h2 className="title">Iniciar Sesión</h2>
+                    <Formik
+                        initialValues={{
+                            email: '',
+                            password: '',
+                        }}
+                        validationSchema={validationSchema}
+                        onSubmit={(values) => {
+                            console.log("IN");
+                            axios.post('http://localhost:5000/Users/get/', {
+                                cliente: values,
+                            })
+                                .then((res) => {
+                                    switch (res.status) {
+                                        case 200:
+                                            console.log(res.data);
+                                            if (res.data !== false) {
+                                                if (res.data.rol === "Administrador") window.localStorage.setItem("rol", 0);
+                                                if (res.data.rol === "Cajero") window.localStorage.setItem("rol", 1);
+                                                if (res.data.rol === "Cliente") window.localStorage.setItem("rol", 2);
+                                                history.push("/Home");
+                                            }
+                                            else alert("Datos ingresados incorrectos");
+                                            break;
 
-                        <div className="input-field">
-                            <div className="loginIcons">
-                                <Mail />
-                            </div>
-                            <input required type="text" placeholder="E-Mail" value={values.mail} onChange={handleMail}></input>
-                        </div>
+                                    }
+                                })
+                                .catch((err) => {
+                                    alert(JSON.stringify(err));
+                                })
+                        }}
+                    >
+                        {({ errors, touched }) => (
+                            <Form>
 
-                        <div className="input-field">
-                            <div className="loginIcons">
-                                <Lock />
-                            </div>
-                            <input required type="password" placeholder="DNI (sin puntuaciones)" value={values.pass} onChange={handlePass}></input>
-                        </div>
+                                <Field placeholder="E-Mail" id="email" name="email" className='input-field' />
+                                {errors.email && touched.email ? (
+                                    <div>{errors.email}</div>
+                                ) : null}
 
-                        <input type="submit" value="Login" class="btnLogin solid"></input>
 
-                    </form>
+                                <Field placeholder="Contraseña" id="password" name="password" className='input-field' />
+                                {errors.password && touched.password ? (
+                                    <div>{errors.password}</div>
+                                ) : null}
+
+
+                                <button type="submit">Submit</button>
+                            </Form>
+                        )}
+
+                    </Formik>
+                    <Button class="btnLogin solid" onClick={() => { history.push('/Register') }}>Registrarse</Button>
                 </div>
             </div>
         </body>
