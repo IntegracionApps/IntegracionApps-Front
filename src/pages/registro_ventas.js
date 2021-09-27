8852a67ed54edd2ba07d1392ec93554f2398da86
MelodRAMAtick <melodramatick@users.noreply.github.com>
2021-09-27 14:21:46 -0300

    Sales Registry Clean up

--- src/pages/registro_ventas.js
+++ src/pages/registro_ventas.js
@@ -6,88 +6,6 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
 
-function Row(props) {
-    const { row } = props;
-    const [open, setOpen] = React.useState(false);
-
-    console.log(row);
-
-    return (
-        <React.Fragment>
-            <TableRow>
-                <TableCell>
-                    <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
-                        {open ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
-                    </IconButton>
-                </TableCell>
-
-                <TableCell component="th" scope="row">{row.id.toString().padStart(10, "0")}</TableCell>
-                <TableCell align="right">{row.items.length} Ítems vendidos</TableCell>
-                <TableCell align="right">AR$ {row.total}</TableCell>
-                <TableCell align="right">{row.medioPago}</TableCell>
-                <TableCell align="right">{row.estado}</TableCell>
-            </TableRow>
-            <TableRow>
-                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-                    <Collapse in={open} timeout="auto" unmountOnExit>
-                        <Box margin={1}>
-                            <Typography variant="h4" gutterBottom component="div">
-                                Detalle
-                            </Typography>
-                            <div style={{ display: "flex", flexDirection: "row", justifyContent: "start" }}>
-                                <Typography variant="h6" gutterBottom style={{ marginRight: "2.5%" }}>
-                                    {row.sucursal.nombre}
-                                </Typography>
-                                <Typography variant="h6" gutterBottom >
-                                    Franquicia : {row.sucursal.direccion}
-                                </Typography>
-                            </div>
-                            <div style={{ display: "flex", flexDirection: "row", justifyContent: "start" }}>
-                                <Typography variant="h6" gutterBottom style={{ marginRight: "2.5%" }}>
-                                    Fecha de Venta : {new Date(row.fechaEmision).toLocaleString()}
-                                </Typography>
-                                <Typography variant="h6" gutterBottom >
-                                    Cliente : {row.cliente.nombre} {row.cliente.apellido}
-                                </Typography>
-                            </div>
-                            <div style={{ display: "flex", flexDirection: "row", justifyContent: "start" }}>
-                                <Typography variant="h6" gutterBottom style={{ marginRight: "2.5%" }}>
-                                    {row.otros1}
-                                </Typography>
-                                <Typography variant="h6" gutterBottom >
-                                    {row.otros2}
-                                </Typography>
-                            </div>
-                            <Table size="small" aria-label="purchases">
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>Descripción</TableCell>
-                                        <TableCell >Precio Unitario</TableCell>
-                                        <TableCell>Cantidad</TableCell>
-                                        <TableCell >Total</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {row.items.map((itemDetailRow) => (
-                                        <TableRow key={itemDetailRow.id}>
-                                            {/* <TableCell component="th" scope="row">{row.fechaEmision}</TableCell>
-                                            <TableCell align="left">{row.cliente.name} {row.cliente.lastName}</TableCell> */}
-                                            <TableCell align="left">{itemDetailRow.descrip}</TableCell>
-                                            <TableCell>AR$ {itemDetailRow.price}</TableCell>
-                                            <TableCell align="left">{itemDetailRow.cantidad} [ {itemDetailRow.tipoUnidad} ]</TableCell>
-                                            <TableCell align="left">AR$ {itemDetailRow.importe}</TableCell>
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        </Box>
-                    </Collapse>
-                </TableCell>
-            </TableRow>
-        </React.Fragment>
-    );
-
-}
 
 const columns = [
     { title: "N° de Venta", field: "id", editable: "never" },
@@ -105,6 +23,11 @@ export default function RegistroVentas() {
     const [ventas, setVentas] = useState([]);
 
     useEffect(() => {
+        
+        // axios.get("http://localhost:5000/Sales/get/all",{
+        //     role: parseInt(window.localStorage.getItem("rol")),
+        // })
+
         axios.get("http://localhost:5000/Sales/get/all")
             .then(function (response) {
                 // handle success
@@ -251,40 +174,8 @@ export default function RegistroVentas() {
                         searchPlaceholder: 'Buscar'
                     }
                 }}
-
-
-
-            // components={{
-            //     EditRow: props => (
-            //         <div style={{ backgroundColor: '#e8eaf5' }}>
-            //             <MTableEditRow {...props} />
-            //         </div>
-            //     )
-            // }}
-
-
-
             />
 
-            {/* <TableContainer>
-                <Table aria-label="collapsible table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell />
-                            <TableCell>N° de Venta</TableCell>
-                            <TableCell align="right">Cantidad de Ítems vendidos</TableCell>
-                            <TableCell align="right">Total de la Venta</TableCell>
-                            <TableCell align="right">Medio de Pago</TableCell>
-                            <TableCell align="right">Estado de la Venta</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {ventas.map((row) => (
-                            <Row key={row.name} row={row} />
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer> */}
             <Link to="/Home">Volver a la tienda</Link>
         </div>
     )
