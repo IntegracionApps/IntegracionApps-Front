864c0ecfa035f9c50a1bfff8b8d29417793819a5
MelodRAMAtick <melodramatick@users.noreply.github.com>
2021-10-01 02:18:34 -0300

    Success dialog + auto-exit

--- src/pages/empleados.js
+++ src/pages/empleados.js
@@ -1,13 +1,17 @@
+import { Dialog, DialogTitle, DialogContent, Typography, DialogActions, Button } from "@material-ui/core";
+import { AttachMoney } from "@material-ui/icons";
 import axios from "axios";
-import MaterialTable, { MTableEditRow } from "material-table";
+import MaterialTable from "material-table";
 import React, { useEffect, useState } from "react";
 import DialogAdd from "../components/AddUserDialog";
 import Header from "../components/Header";
-import ListaEmpleados from "../components/ListaEmpleador";
 
 export default function Empleados() {
     const [data, setData] = useState([]);
     const [visible, setVisible] = useState(false);
+    
+    const [successRegister, setSuccessRegister] = useState(false);
+    
     useEffect(() => {
         const axios = require('axios');
         axios.get("http://localhost:5000/Users/get/all")
@@ -47,14 +51,13 @@ export default function Empleados() {
     const columns = [
         { title: "Nombre", field: "nombre", editable: "never" },
         { title: "Apellido", field: "apellido", editable: "never" },
-        { title: "Rol", field: "rol" },
+        { title: "Rol", field: "rol", defaultSort: "asc" },
         { title: "Mail", field: "email" },
         { title: "Dirección", field: "ubicacion.direccion" },
         { title: "Altura", field: "ubicacion.altura" },
         { title: "Piso", field: "ubicacion.piso" },
         { title: "Teléfono", field: "telefono" },
         { title: "Salario ($)", field: "salario" },
-
     ]
 
     function handleDelete(empleado) {
@@ -92,118 +95,139 @@ export default function Empleados() {
                         tooltip: "Crear Empleado",
                         isFreeAction: true,
                         onClick: () => setVisible(true),
-                    }
+                    },
+                    rowData => ({
+                        icon: () => <AttachMoney />,
+                        tooltip: "Pagar Empleado",
+                        onClick: (event, rowData) => { alert("Se le han pagado $" + rowData.salario + " al empleado " + rowData.nombre + " " + rowData.apellido) },
+                    }),
+
                 ]}
                 options={{
                     actionsColumnIndex: -1
                 }}
 
-                
+
                 editable={{
                     onRowUpdate: (newData, oldData) =>
-                    new Promise((resolve, reject) => {
-                        setTimeout(() => {
-                            const dataUpdate = [...data];
-                            const index = oldData.tableData.id;
-                            dataUpdate[index] = newData;
-                            setData([...dataUpdate]);
-                            
-                            console.log(dataUpdate[index]);
-                            axios.post("http://localhost:5000/Users/edit/employee", {
-                                empleado: dataUpdate[index],
-                            })
-                            .then((res) => {
-                                console.log(res.data);
-                                console.log(res.status + ": " + res.statusText);
-                            })
-                            .catch((err) => {
-                                console.log(err);
-                            })
-                            
-                            
-                            resolve();
-                        }, 1000)
-                    }),
-                    onRowDelete: oldData =>
-                    new Promise((resolve, reject) => {
-                        setTimeout(() => {
-                            const dataDelete = [...data];
-                            const index = oldData.tableData.id;
-                            console.log(typeof (oldData.id));
-                            dataDelete.splice(index, 1);
-                            setData([...dataDelete]);
-                            axios.post("http://localhost:5000/Users/delete/" + oldData.id)
-                            .then((res) => {
-                                console.log(res.data);
+                        new Promise((resolve, reject) => {
+                            setTimeout(() => {
+                                const dataUpdate = [...data];
+                                const index = oldData.tableData.id;
+                                dataUpdate[index] = newData;
+                                setData([...dataUpdate]);
+
+                                console.log(dataUpdate[index]);
+                                axios.post("http://localhost:5000/Users/edit/employee", {
+                                    empleado: dataUpdate[index],
+                                })
+                                    .then((res) => {
+                                        console.log(res.data);
                                         console.log(res.status + ": " + res.statusText);
                                     })
                                     .catch((err) => {
                                         console.log(err);
                                     })
-                                    
-                                    
-                                    resolve();
-                                }, 1000)
-                            }),
-                            
-                        }}
-                        
-                        
-                        localization={{
-                            body: {
-                                emptyDataSourceMessage: "Sin Empleados!",
-                                deleteTooltip: 'Dar de  Baja',
-                                editTooltip: 'Editar',
-                                filterRow: {
-                                    filterTooltip: 'Filtrar'
-                                },
-                                editRow: {
-                                    deleteText: '¿Seguro que quiere dar de baja a este empleado?',
-                                    cancelTooltip: 'Cancelar',
-                                    saveTooltip: 'Confirmar'
-                                }
-                            },
-                            header: {
-                                actions: 'Opciones'
-                            },
-                            pagination: {
-                                labelDisplayedRows: '{from}-{to} de {count}',
-                                labelRowsSelect: 'filas',
-                                labelRowsPerPage: 'filas por página:',
-                                firstAriaLabel: 'Primera Página',
-                                firstTooltip: 'Primera Página',
-                                previousAriaLabel: 'Página Anterior',
-                                previousTooltip: 'Página Anterior',
-                                nextAriaLabel: 'Página Siguiente',
-                                nextTooltip: 'Página Siguiente',
-                                lastAriaLabel: 'Última Página',
-                                lastTooltip: 'Última Página'
-                            },
-                            toolbar: {
-                                addRemoveColumns: 'Agregar o Eliminar Columnas',
-                                nRowsSelected: '{0} fila(s) seleccionada(s)',
-                                showColumnsTitle: 'Mostrar Columnas',
-                                showColumnsAriaLabel: 'Mostrar Columnas',
-                                searchTooltip: 'Buscar',
-                                searchPlaceholder: 'Buscar'
-                            }
-                        }}
-                                    
-                        
-                        
-                        // components={{
-                            //     EditRow: props => (
-                                //         <div style={{ backgroundColor: '#e8eaf5' }}>
-                                //             <MTableEditRow {...props} />
-                                //         </div>
-                                //     )
+
+
+                                resolve();
+                            }, 1000)
+                        }),
+                    onRowDelete: oldData =>
+                        new Promise((resolve, reject) => {
+                            setTimeout(() => {
+                                const dataDelete = [...data];
+                                const index = oldData.tableData.id;
+                                console.log(typeof (oldData.id));
+                                dataDelete.splice(index, 1);
+                                setData([...dataDelete]);
+                                axios.post("http://localhost:5000/Users/delete/" + oldData.id)
+                                    .then((res) => {
+                                        console.log(res.data);
+                                        console.log(res.status + ": " + res.statusText);
+                                    })
+                                    .catch((err) => {
+                                        console.log(err);
+                                    })
+
+
+                                resolve();
+                            }, 1000)
+                        }),
+
+                }}
+
+
+                localization={{
+                    body: {
+                        emptyDataSourceMessage: "Sin Empleados!",
+                        deleteTooltip: 'Dar de  Baja',
+                        editTooltip: 'Editar',
+                        filterRow: {
+                            filterTooltip: 'Filtrar'
+                        },
+                        editRow: {
+                            deleteText: '¿Seguro que quiere dar de baja a este empleado?',
+                            cancelTooltip: 'Cancelar',
+                            saveTooltip: 'Confirmar'
+                        }
+                    },
+                    header: {
+                        actions: 'Opciones'
+                    },
+                    pagination: {
+                        labelDisplayedRows: '{from}-{to} de {count}',
+                        labelRowsSelect: 'filas',
+                        labelRowsPerPage: 'filas por página:',
+                        firstAriaLabel: 'Primera Página',
+                        firstTooltip: 'Primera Página',
+                        previousAriaLabel: 'Página Anterior',
+                        previousTooltip: 'Página Anterior',
+                        nextAriaLabel: 'Página Siguiente',
+                        nextTooltip: 'Página Siguiente',
+                        lastAriaLabel: 'Última Página',
+                        lastTooltip: 'Última Página'
+                    },
+                    toolbar: {
+                        addRemoveColumns: 'Agregar o Eliminar Columnas',
+                        nRowsSelected: '{0} fila(s) seleccionada(s)',
+                        showColumnsTitle: 'Mostrar Columnas',
+                        showColumnsAriaLabel: 'Mostrar Columnas',
+                        searchTooltip: 'Buscar',
+                        searchPlaceholder: 'Buscar'
+                    }
+                }}
+
+
+
+            // components={{
+            //     EditRow: props => (
+            //         <div style={{ backgroundColor: '#e8eaf5' }}>
+            //             <MTableEditRow {...props} />
+            //         </div>
+            //     )
             // }}
 
 
 
             />
             {/* <ListaEmpleados employee_data={data} visible={visible} /> */}
-            < DialogAdd open={visible} onClose={() => { setVisible(false) }} />
+            < DialogAdd open={visible} onClose={() => { setVisible(false) }} onSuccess={() => {setSuccessRegister(true)}} />
+
+            <Dialog open={successRegister} onClose={() => { setSuccessRegister(false) }}>
+                <DialogTitle>¡Éxito!</DialogTitle>
+                <DialogContent>
+                    <Typography>Su registración fue realizada con éxito</Typography>
+                    {/* <Typography>¡Bienvenido!</Typography> */}
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => {
+                        setSuccessRegister(false);
+                        setVisible(false);
+                    }}>Regresar a la Tabla</Button>
+                </DialogActions>
+            </Dialog>
+
         </div>
     )
 }
